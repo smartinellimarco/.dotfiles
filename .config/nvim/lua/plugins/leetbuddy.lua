e14b162f3503c6be6c50def89ef773d61ef89664
smartinellimarco <smartinellimarco@gmail.com>
2023-10-01 14:10:32 -0300

    Set keymaps in setup function

--- .config/nvim/lua/plugins/leetbuddy.lua
+++ .config/nvim/lua/plugins/leetbuddy.lua
@@ -1,15 +1,13 @@
 local M = { 'Dhanus3133/LeetBuddy.nvim' }
 
-M.keys = {
-  { '<leader>lq', '<cmd>LBQuestions<cr>', desc = 'List Questions' },
-  { '<leader>ll', '<cmd>LBQuestion<cr>',  desc = 'View Question' },
-  { '<leader>lr', '<cmd>LBReset<cr>',     desc = 'Reset Code' },
-  { '<leader>lt', '<cmd>LBTest<cr>',      desc = 'Run Code' },
-  { '<leader>ls', '<cmd>LBSubmit<cr>',    desc = 'Submit Code' },
-}
-
 function M.config(_, opts)
   require('leetbuddy').setup(opts)
+
+  vim.keymap.set('n', '<leader>lq', '<cmd>LBQuestions<cr>', { desc = 'List Questions' })
+  vim.keymap.set('n', '<leader>ll', '<cmd>LBQuestion<cr>', { desc = 'View Question' })
+  vim.keymap.set('n', '<leader>lr', '<cmd>LBReset<cr>', { desc = 'Reset Code' })
+  vim.keymap.set('n', '<leader>lt', '<cmd>LBTest<cr>', { desc = 'Run Code' })
+  vim.keymap.set('n', '<leader>ls', '<cmd>LBSubmit<cr>', { desc = 'Submit Code' })
 end
 
 return M
