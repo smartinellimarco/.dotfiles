65f992e7329d4442c05e4641a2861854fe24159c
smartinellimarco <smartinellimarco@gmail.com>
2023-10-17 11:01:52 -0300

    Add surround plugin back

---
+++ .config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,13 @@
+local M = { 'kylechui/nvim-surround' }
+
+M.version = '*'
+M.event = 'VeryLazy'
+M.opts = {}
+
+function M.config(_, opts)
+  require('nvim-surround').setup(opts)
+end
+
+return M
+
+-- vim: ts=2 sts=2 sw=2 et
