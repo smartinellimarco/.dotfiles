9f69de471dacd0fe3419bcd7b7d898c3bc8e238f
smartinellimarco <smartinellimarco@gmail.com>
2023-11-02 17:48:57 -0300

    Set a single multiwindow and bidirectional jumping keymap

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,7 +1,13 @@
 local M = { 'ggandor/leap.nvim' }
 
 function M.config(_, _)
-  require("leap").add_default_mappings()
+  vim.keymap.set({'n', 'v'}, '<leader><leader>', function ()
+    local focusable_windows_on_tabpage = vim.tbl_filter(
+      function (win) return vim.api.nvim_win_get_config(win).focusable end,
+      vim.api.nvim_tabpage_list_wins(0)
+    )
+    require('leap').leap { target_windows = focusable_windows_on_tabpage }
+  end)
 end
 
 return M
