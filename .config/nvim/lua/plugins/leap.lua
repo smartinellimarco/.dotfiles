8c8471ffb5aa97b013fc2504f63c5d82d33300ad
smartinellimarco <smartinellimarco@gmail.com>
2023-11-14 19:36:47 -0300

    Update the windows list on each leap call

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,27 +1,30 @@
 local M = { 'ggandor/leap.nvim' }
 
--- Create a filtered list of focusable windows on the current tabpage
-M.focusable_windows_on_tabpage = vim.tbl_filter(
+function M.focusable_windows_on_tabpage()
+  -- Create a filtered list of focusable windows on the current tabpage
+  local windows = vim.tbl_filter(
 
-  -- Function that checks if a window is focusable
-  function(win)
-    return vim.api.nvim_win_get_config(win).focusable
-  end,
+  -- Function that checks if window is focusable
+    function(win)
+      return vim.api.nvim_win_get_config(win).focusable
+    end,
 
-  -- Retrieve a list of all windows on the current tabpage
-  vim.api.nvim_tabpage_list_wins(0)
-)
+    -- Retrieve a list of all windows on the current tabpage
+    vim.api.nvim_tabpage_list_wins(0)
+  )
+  return windows
+end
 
 M.opts = {
   special_keys = {
     next_group = '<space>',
     prev_group = '<tab>',
-  },
-  target_windows = M.focusable_windows_on_tabpage,
+  }
 }
 
 function M.config(_, opts)
-  vim.keymap.set({ 'n', 'v' }, 's', function() require('leap').leap(opts) end)
+  vim.keymap.set({ 'n', 'v' }, 's',
+    function() require('leap').leap({ opts, target_windows = M.focusable_windows_on_tabpage() }) end)
 end
 
 return M
