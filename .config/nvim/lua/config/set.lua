21ebd456022baca3b732ef9f31b56d825238a5be
smartinellimarco <smartinellimarco@gmail.com>
2023-11-14 18:30:14 -0300

    Remove command and status line options

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -25,8 +25,6 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.clipboard = 'unnamedplus'
 vim.o.wrap = false
 vim.o.undofile = true
-vim.o.cmdheight = 0
-vim.o.laststatus = 0
 vim.o.signcolumn = 'yes:2'
 vim.o.splitright = true
 
@@ -36,5 +34,8 @@ vim.api.nvim_command("autocmd VimEnter * exe 'clearjumps'")
 -- Launch telescope file picker and change dir when opening neovim
 -- with a directory argument
 vim.api.nvim_command("autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) | execute 'Telescope find_files' | execute 'cd '.argv()[0] | endif")
+
+-- Open help window in a vertical split to the right.
+vim.api.nvim_command("autocmd BufEnter * if &filetype=='help' | wincmd L | endif")
 
 -- vim: ts=2 sts=2 sw=2 et
