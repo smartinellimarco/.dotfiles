ba2aff89d9593c5056d2eae5d49fe6a5059ae4c1
smartinellimarco <smartinellimarco@gmail.com>
2023-11-20 18:45:30 -0300

    Enhance virtual text formatting to align with the format used by the LSPs

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,12 @@ local M = { 'lewis6991/gitsigns.nvim' }
 
 M.opts = {
   current_line_blame = true,
+  current_line_blame_opts = {
+    ignore_whitespace = false,
+    virt_text_priority = 5000, -- LSP diagnostics priority is 4096 (0x1000)
+  },
+  current_line_blame_formatter = '◦ <author>, <author_time:%Y-%m-%d> - <summary>',
+  current_line_blame_formatter_nc = '◦ Not committed yet',
   yadm = {
     enable = true
   }
