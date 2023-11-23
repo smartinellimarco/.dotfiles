2fe2f5a8dcb35a1e4a33f0ce05d0c98a8e2b0043
smartinellimarco <smartinellimarco@gmail.com>
2023-11-23 10:02:19 -0300

    Modularize the LSP config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,113 +1,103 @@
-return {
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
+local M = { 'neovim/nvim-lspconfig' }
 
-    -- Disable automatic setup, we are doing it manually
-    init = function()
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    opts = {
-      ui = {
-        border = "rounded"
-      }
-    }
-  },
-  {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      { 'williamboman/mason-lspconfig.nvim' },
-    },
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      local telescope = require('telescope.builtin')
+M.cmd = { 'LspInfo', 'LspInstall', 'LspStart' }
+M.event = { 'BufReadPre', 'BufNewFile' }
+M.dependencies = {
 
-      -- This adds the cmp capabilities to the lsp servers
-      -- Also enables the '.set_server_config' function
-      lsp_zero.extend_lspconfig()
+  -- LSP manager
+  { import = 'plugins.lsp.mason' },
 
-      lsp_zero.on_attach(function(client, bufnr)
-        vim.keymap.set('n', 'gd', telescope.lsp_definitions, { buffer = bufnr })
-        vim.keymap.set('n', 'gl', telescope.diagnostics, { buffer = bufnr })
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
-        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = bufnr })
-        vim.keymap.set('n', '<leader>c', vim.lsp.buf.code_action, { buffer = bufnr })
-      end)
+  -- Helper LSP setup tool
+  { import = 'plugins.lsp.lsp_zero' },
 
-      -- Disable semantic highlights to avoid conflicts with treesitter
-      -- Even though both provide highlights, treesitter works better in
-      -- most scenarios.
-      lsp_zero.set_server_config({
-        on_init = function(client)
-          client.server_capabilities.semanticTokensProvider = nil
-        end,
-      })
+  -- Show function signatures while typing
+  { import = 'plugins.lsp.lsp_signature' },
 
-      -- Floating messages turned off
-      vim.diagnostic.config({
-        virtual_text = false,
-      })
-
-      -- Icons for diagnostics
-      lsp_zero.set_sign_icons({
-        error = '',
-        warn = '',
-        hint = '',
-        information = '',
-      })
-
-      -- Ensure the servers are installed
-      require('mason-lspconfig').setup({
-        ensure_installed = {
-          'lua_ls',
-          'jsonls',
-          'terraformls',
-          'marksman',
-          'pyright',
-          'clangd',
-          'docker_compose_language_service',
-        },
-        handlers = {
-
-          -- Configs for each LSP can be placed here
-
-          -- Setup lua_ls with neovim API compatibility
-          -- using lsp_zero options for lspconfig
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-
-          -- Setup a less strict pyright
-          pyright = function()
-            require('lspconfig').pyright.setup({
-              settings = {
-                python = {
-                  analysis = {
-                    diagnosticMode = 'workspace',
-                    typeCheckingMode = 'off'
-                  }
-                }
-              }
-            })
-          end,
-
-          -- Use the default setup on the remaining servers
-          lsp_zero.default_setup,
-
-        }
-      })
-    end
-  }
 }
+
+function M.config(_, _)
+  local lsp_zero = require('lsp-zero')
+  local lspconfig = require('lspconfig')
+  local mason_lspconfig = require('mason-lspconfig')
+
+  -- This adds the cmp capabilities to the lsp servers
+  -- Also enables the '.set_server_config' function
+  lsp_zero.extend_lspconfig()
+
+  lsp_zero.on_attach(
+    function(_, bufnr)
+      lsp_zero.default_keymaps({ buffer = bufnr })
+    end
+  )
+
+  -- Disable semantic highlights to avoid conflicts with treesitter
+  -- Even though both provide highlights, treesitter works better in
+  -- most scenarios.
+  lsp_zero.set_server_config({
+    on_init = function(client)
+      client.server_capabilities.semanticTokensProvider = nil
+    end,
+  })
+
+  vim.diagnostic.config({
+    virtual_text = {
+      prefix = '◦', -- Change icon
+      spacing = 0,
+    },
+    signs = false, -- Disable sign column icons
+  })
+
+  -- Icons for diagnostics
+  lsp_zero.set_sign_icons({
+    error = '',
+    warn = '',
+    hint = '',
+    information = '',
+  })
+
+  -- Ensure the servers are installed
+  mason_lspconfig.setup({
+    ensure_installed = {
+      'lua_ls',
+      'jsonls',
+      'terraformls',
+      'marksman',
+      'pyright',
+      'clangd',
+      'docker_compose_language_service',
+    },
+    handlers = {
+
+      -- Configs for each LSP can be placed here
+
+      -- Setup lua_ls with neovim API compatibility
+      -- using lsp_zero options for lspconfig
+      lua_ls = function()
+        local lua_opts = lsp_zero.nvim_lua_ls()
+        lspconfig.lua_ls.setup(lua_opts)
+      end,
+
+      -- Setup a less strict pyright
+      pyright = function()
+        lspconfig.pyright.setup({
+          settings = {
+            python = {
+              analysis = {
+                diagnosticMode = 'workspace',
+                typeCheckingMode = 'off'
+              }
+            }
+          }
+        })
+      end,
+
+      -- Use the default setup on the remaining servers
+      lsp_zero.default_setup,
+
+    }
+  })
+end
+
+return M
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ .config/nvim/lua/plugins/lsp/lsp_signature.lua
@@ -0,0 +1,20 @@
+local M = { 'ray-x/lsp_signature.nvim' }
+
+-- This plugin must load after the LSP
+M.event = "VeryLazy"
+M.opts = {
+  bind = true,
+  handler_opts = {
+    border = "rounded"
+  },
+  hint_enable = false,
+  always_trigger = true
+}
+
+function M.config(_, opts)
+  require('lsp_signature').setup(opts)
+end
+
+return M
+
+-- vim: ts=2 sts=2 sw=2 et
