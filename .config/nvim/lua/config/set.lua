8af8b01b782ced545d1939c10f4fe3189361bad7
smartinellimarco <smartinellimarco@gmail.com>
2023-12-03 21:14:32 -0300

    Move cmp settings to sets file

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -2,24 +2,26 @@
 vim.g.mapleader = ' '
 
 -- Configs
-vim.o.mouse = ''                          -- Disable mouse
-vim.o.scrolloff = 0                       -- No scrolling offset
-vim.wo.number = true                      -- Show line numbers
-vim.o.termguicolors = true                -- Enable truecolor
-vim.o.hlsearch = false                    -- Highlight search results
-vim.o.incsearch = true                    -- Search incrementally
-vim.o.ignorecase = true                   -- Case insensitive search
-vim.o.smartcase = true                    -- Unless capital in search
-vim.o.wildmode = 'list:longest'           -- TODO: review
-vim.o.clipboard = 'unnamedplus'           -- Enable OS clipboard
-vim.o.wrap = false                        -- Do not wrap lines
-vim.o.undofile = true                     -- Persist the undo history
-vim.o.signcolumn = 'yes:1'                -- Use a signcolumn with 1 space for git status
-vim.o.splitright = true                   -- Open vertical splits to the right side of the screen
-vim.opt.cmdheight = 0                     -- Hide command line
-vim.opt.showmode = false                  -- Do not show neovim mode
-vim.opt.shortmess = vim.opt.shortmess + { -- Reduce messages
-  -- TODO: suppress some of these
+vim.o.mouse = ''                              -- Disable mouse
+vim.o.scrolloff = 0                           -- No scrolling offset
+vim.wo.number = true                          -- Show line numbers
+vim.o.termguicolors = true                    -- Enable truecolor
+vim.o.hlsearch = false                        -- Highlight search results
+vim.o.incsearch = true                        -- Search incrementally
+vim.o.ignorecase = true                       -- Case insensitive search
+vim.o.smartcase = true                        -- Unless capital in search
+vim.o.wildmode = 'list:longest'               -- TODO: review
+vim.o.clipboard = 'unnamedplus'               -- Enable OS clipboard
+vim.o.wrap = false                            -- Do not wrap lines
+vim.o.undofile = true                         -- Persist the undo history
+vim.o.signcolumn = 'yes:1'                    -- Use a signcolumn with 1 space for git status
+vim.o.splitright = true                       -- Open vertical splits to the right side of the screen
+vim.opt.cmdheight = 0                         -- Hide command line
+vim.opt.showmode = false                      -- Do not show neovim mode
+vim.opt.complete = ''                         -- Disable builtin i_CTRL-N and i_CTRL-P in favor of nvim-cmp
+vim.opt.completeopt = 'menu,menuone,noselect' -- Options for using a completion menu, similar to other IDEs
+vim.opt.shortmess = vim.opt.shortmess + {     -- Reduce messages
+  -- TODO: find a way to supress these
   F = true,
   W = true,
   I = true,
