863518a4f86aaa476e382b68d06d4da4c8bbdf6b
smartinellimarco <smartinellimarco@gmail.com>
2023-12-03 21:15:09 -0300

    Remove this autocmd due to conflicts with telescope

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -5,8 +5,4 @@ vim.api.nvim_command("autocmd VimEnter * exe 'clearjumps'")
 -- with a directory argument
 vim.api.nvim_command("autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) | execute 'Telescope find_files' | execute 'cd '.argv()[0] | endif")
 
--- Open help window in a vertical split to the right.
--- TODO: this flickers
-vim.api.nvim_command("autocmd BufEnter * if &filetype=='help' | wincmd L | endif")
-
 -- vim: ts=2 sts=2 sw=2 et
