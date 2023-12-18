f3ee56e96c70d5b4be4121edec23db8fa5eab494
smartinellimarco <smartinellimarco@gmail.com>
2023-12-18 17:55:24 -0300

    Toggle line numbers on/off depending on the current mode

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -32,4 +32,27 @@ vim.opt.shortmess = vim.opt.shortmess + {     -- Reduce messages
   A = true,
 }
 
+-- Turn relative numbers on/off depending on the current mode
+local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+         vim.opt.relativenumber = true
+      end
+   end,
+})
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu then
+         vim.opt.relativenumber = false
+         vim.cmd "redraw"
+      end
+   end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
