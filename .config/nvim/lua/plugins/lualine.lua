ffd7af187d19c16d717a74449340cf0ce83f9768
smartinellimarco <smartinellimarco@gmail.com>
2023-12-28 21:53:21 -0300

    Make lualine show the relative file path

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ M.opts = {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1 } },
     lualine_x = { 'encoding' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
