ab8bf5c27f5a38a3c209b4d736ee1fab73be3942
smartinellimarco <smartinellimarco@gmail.com>
2023-12-28 21:59:06 -0300

    Improve code

--- .config/nvim/lua/plugins/lsp/on_attach/keymaps.lua
+++ .config/nvim/lua/plugins/lsp/on_attach/keymaps.lua
@@ -1,20 +1,30 @@
 local M = {}
 
 function M.setup(bufnr)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = bufnr })
-  vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, { buffer = bufnr })
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr })
-  vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { buffer = bufnr })
-  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = bufnr })
-  vim.keymap.set({ 'n', 'x' }, '<leader>i', function() vim.lsp.buf.format({ async = true }) end, { buffer = bufnr })
-  vim.keymap.set('n', '<leader>c', vim.lsp.buf.code_action, { buffer = bufnr })
-  vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { buffer = bufnr })
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = bufnr })
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = bufnr })
-  vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, { buffer = bufnr })
+  local bufmap = function(mode, lhs, rhs)
+    local opts = { buffer = bufnr }
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+
+  bufmap('n', 'K', vim.lsp.buf.hover)
+  bufmap('n', 'gd', vim.lsp.buf.definition)
+  bufmap('n', 'gD', vim.lsp.buf.declaration)
+  bufmap('n', 'gi', vim.lsp.buf.implementation)
+  bufmap('n', 'go', vim.lsp.buf.type_definition)
+  bufmap('n', 'gr', vim.lsp.buf.references)
+  bufmap('n', 'gs', vim.lsp.buf.signature_help)
+  bufmap('n', '<leader>r', vim.lsp.buf.rename)
+  bufmap({ 'n', 'x' }, '<leader>i',
+    function()
+      -- see: https://github.com/neovim/neovim/pull/26549
+      vim.lsp.buf.format({ async = true })
+    end
+  )
+  bufmap('n', '<leader>c', vim.lsp.buf.code_action)
+  bufmap('n', 'gl', vim.diagnostic.open_float)
+  bufmap('n', '[d', vim.diagnostic.goto_prev)
+  bufmap('n', ']d', vim.diagnostic.goto_next)
+  bufmap('i', '<C-s>', vim.lsp.buf.signature_help)
 end
 
 return M
