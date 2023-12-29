993cd927aae8a96f0515e476aed2cf5b047d3d9b
smartinellimarco <smartinellimarco@gmail.com>
2023-12-29 14:34:01 -0300

    Add stress reliever plugin

---
+++ .config/nvim/lua/plugins/cellularautomaton.lua
@@ -0,0 +1,17 @@
+local M = { 'eandrju/cellular-automaton.nvim' }
+
+function M.config(_, _)
+  local automaton = require('cellular-automaton')
+  vim.keymap.set(
+    'n',
+    '<leader>fml',
+    function()
+      local animations = { 'make_it_rain', 'game_of_life' }
+      _, _ = pcall(automaton.start_animation, animations[math.random(#animations)])
+    end
+  )
+end
+
+return M
+
+-- vim: ts=2 sts=2 sw=2 et
