99568ee18ba38f0a1490fa84760b20e71291e0da
smartinellimarco <smartinellimarco@gmail.com>
2023-12-28 21:53:00 -0300

    Disable highlights they way the docs specify

--- .config/nvim/lua/plugins/lsp/on_attach/highlights.lua
+++ .config/nvim/lua/plugins/lsp/on_attach/highlights.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 function M.setup(client)
-  client.server_capabilities.semanticTokensProvider = nil
+  -- Hide all semantic highlights
+  for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+    vim.api.nvim_set_hl(0, group, {})
+  end
 end
 
 return M
