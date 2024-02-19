eb1b1237ab3b6b8273a27882537df8fd37bab7b0
smartinellimarco <smartinellimarco@gmail.com>
2024-02-19 18:58:09 -0300

    Remove dressing and add oil

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,27 @@
+local M = { 'stevearc/oil.nvim' }
+
+M.event = 'VeryLazy'
+M.dependencies = {
+  'nvim-tree/nvim-web-devicons',
+}
+M.opts = {
+  keymaps = {
+    ["l"] = "actions.select",
+    ["<CR>"] = "actions.select",
+    ["<C-v>"] = "actions.select_vsplit",
+    ["<C-x>"] = "actions.select_split",
+    ["h"] = "actions.parent",
+    ["."] = "actions.open_cwd",
+    ["g."] = "actions.toggle_hidden",
+  },
+  use_default_keymaps = false,
+}
+
+function M.config(_, opts)
+  require('oil').setup(opts)
+  vim.keymap.set('n', '<leader>o', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+end
+
+return M
+
+-- vim: ts=2 sts=2 sw=2 et
