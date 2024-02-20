441a01e4057cd4a61945e06f8a736b928ce0f87d
smartinellimarco <smartinellimarco@gmail.com>
2024-02-20 17:26:50 -0300

    Remove augroup

--- .config/nvim/lua/plugins/lsp/on_attach/cursorword.lua
+++ .config/nvim/lua/plugins/lsp/on_attach/cursorword.lua
@@ -2,11 +2,9 @@ local M = {}
 
 function M.setup(client, bufnr)
   if client.server_capabilities.documentHighlightProvider then
-    local augroup = vim.api.nvim_create_augroup('cursorword', { clear = false })
 
     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
       buffer = bufnr,
-      group = augroup,
       callback = function()
         pcall(vim.lsp.buf.document_highlight)
       end
@@ -14,7 +12,6 @@ function M.setup(client, bufnr)
 
     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
       buffer = bufnr,
-      group = augroup,
       callback = function()
         pcall(vim.lsp.buf.clear_references)
       end
