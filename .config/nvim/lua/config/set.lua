e96e9ba231fb5a41798c7e8d25463e1d4b461d34
smartinellimarco <smartinellimarco@gmail.com>
2024-02-27 12:08:09 -0300

    Add keymaps for better window movement

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -42,4 +42,10 @@ vim.opt.shortmess = vim.opt.shortmess + { -- Reduce messages
   A = true,
 }
 
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
 -- vim: ts=2 sts=2 sw=2 et
