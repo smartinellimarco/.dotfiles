ba5b235c836b57f7d1123de8eb5a938b81d5f308
smartinellimarco <smartinellimarco@gmail.com>
2024-03-11 00:05:45 -0300

    Add conform config

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+local M = { 'stevearc/conform.nvim' }
+
+M.opts = {
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    python = { 'isort', 'black' },
+  },
+}
+
+function M.config(_, opts)
+  local conform = require('conform')
+
+  conform.setup(opts)
+
+  vim.keymap.set({ 'n', 'x' }, '<leader>i', function()
+    conform.format({
+      lsp_fallback = true,
+      quiet = true,
+    })
+  end)
+end
+
+return M
+
+-- vim: ts=2 sts=2 sw=2 et
