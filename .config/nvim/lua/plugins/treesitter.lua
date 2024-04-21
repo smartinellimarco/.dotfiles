d5877fd90597a83cce634e1a742d97278eae8634
smartinellimarco <smartinellimarco@gmail.com>
2024-04-21 01:23:22 -0300

    Add textobjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ local M = { 'nvim-treesitter/nvim-treesitter' }
 
 M.lazy = false -- Lazy loading treesitter may produce errors
 M.build = ':TSUpdate'
+M.dependencies = 'nvim-treesitter/nvim-treesitter-textobjects'
 M.opts = {
   ensure_installed = 'all',
   highlight = {
@@ -9,6 +10,26 @@ M.opts = {
   },
   indent = {
     enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+        ['al'] = '@loop.outer',
+        ['il'] = '@loop.inner',
+        ['ai'] = '@conditional.outer',
+        ['ii'] = '@conditional.inner',
+        ['ap'] = '@parameter.outer',
+        ['ip'] = '@parameter.inner',
+        ['as'] = '@call.outer',
+        ['is'] = '@call.inner',
+      },
+    },
   },
 }
 
