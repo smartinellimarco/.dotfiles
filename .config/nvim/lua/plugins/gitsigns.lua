f37144b702cdbccdd3eead1acbda8fa035086a18
smartinellimarco <smartinellimarco@gmail.com>
2024-06-23 20:23:34 -0300

    Reduce delay

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,7 +5,7 @@ local M = { 'lewis6991/gitsigns.nvim' }
 M.opts = {
   current_line_blame = true,
   current_line_blame_opts = {
-    delay = 1000,
+    delay = 500,
     ignore_whitespace = false,
     virt_text_priority = 5000, -- LSP diagnostics priority is 4096 (0x1000)
   },
