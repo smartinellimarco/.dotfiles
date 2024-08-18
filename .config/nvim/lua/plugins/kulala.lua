8f12c0d3349484d232295e5de9d971606b2a160b
smartinellimarco <smartinellimarco@gmail.com>
2024-08-18 20:31:16 -0300

    Add kulala plugin

---
+++ .config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,24 @@
+local M = { 'mistweaverco/kulala.nvim' }
+
+M.event = 'VeryLazy'
+M.opts = {
+  show_icons = nil,
+  icons = {
+    inlay = {
+      done = '',
+      loading = '󰦖',
+      error = '',
+    },
+    lualine = '',
+  },
+}
+
+function M.config(_, opts)
+  require('kulala').setup(opts)
+  -- TODO: define keymaps
+  -- see: https://github.com/mistweaverco/kulala-fmt/issues/12
+end
+
+return M
+
+-- vim: ts=2 sts=2 sw=2 et
