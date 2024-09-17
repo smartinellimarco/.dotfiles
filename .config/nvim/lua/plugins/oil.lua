9452c3a6e009aed238880fac2d1f5a517ba4e3c4
smartinellimarco <smartinellimarco@gmail.com>
2024-09-17 13:30:29 -0300

    Update keymaps

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,25 +1,20 @@
+-- TODO: https://github.com/stevearc/oil.nvim/issues/363
 local M = { 'stevearc/oil.nvim' }
 
-M.dependencies = {
-  'nvim-tree/nvim-web-devicons',
-}
+M.dependencies = { 'nvim-tree/nvim-web-devicons' }
 M.opts = {
   view_options = {
     show_hidden = true,
   },
   keymaps = {
-    ['l'] = 'actions.select',
-    ['h'] = 'actions.parent',
+    ['<CR>'] = 'actions.select',
+    ['-'] = 'actions.parent',
+    ['_'] = 'actions.open_cwd',
+    ['`'] = 'actions.cd',
     ['<C-v>'] = 'actions.select_vsplit',
     ['<C-x>'] = 'actions.select_split',
-    ['<C-q>'] = {
-      callback = function()
-        require('oil.actions').send_to_qflist.callback()
-        vim.cmd.copen()
-      end,
-    },
-    ['`'] = 'actions.cd',
-    ['<leader>o'] = 'actions.close', -- Act as toggle
+    ['<C-c>'] = 'actions.close',
+    ['<leader>o'] = function() end, -- Disable keymap
   },
   skip_confirm_for_simple_edits = false,
   prompt_save_on_select_new_entry = true,
