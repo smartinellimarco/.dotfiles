2d0d94cfb143619d8678defa9a77303d288e0c55
smartinellimarco <smartinellimarco@gmail.com>
2024-10-14 11:56:01 -0300

    Change warning to note

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- If neovim is started from the '/home' or '~/.config' directories
 -- assume that GIT_DIR points to the yadm repository
--- WARNING: This is done so lualine shows the branch of the yadm repository
+-- NOTE: This is done so lualine shows the branch of the yadm repository
 -- and gitsigns shows the changes
 if vim.env.GIT_DIR == nil then
   local cwd = vim.fn.getcwd()
