f117eabecded343f85ed08f9f23ef79aadd8f452
smartinellimarco <smartinellimarco@icloud.com>
2024-11-28 01:57:07 -0300

    Remove TODO

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 local M = { 'williamboman/mason-lspconfig.nvim' }
 
 M.cmd = { 'LspInfo', 'LspInstall', 'LspStart' }
-M.event = { 'BufReadPost', 'BufNewFile' } -- TODO: BufReadPost?
+M.event = { 'BufReadPost', 'BufNewFile' }
 M.dependencies = {
   -- Helper plugin for configuring LSPs
   {
